050d21ea4631d9450935db23c535a47342b18bd2
rguillome <dev@guillaumerenaudin.com>
2018-10-03 19:57:15 +0200

    Correction of CORS and Database init

--- src/main/java/fr/formation/config/WebMvcConfig.java
+++ src/main/java/fr/formation/config/WebMvcConfig.java
@@ -4,15 +4,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * Spring Web MVC Configuration. This method replaces the traditional web.xml deployment descriptor.
+ * Spring Web MVC Configuration. This method replaces the traditional web.xml
+ * deployment descriptor.
  * 
  *
  */
 public class WebMvcConfig implements WebMvcConfigurer {
-	
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS")
+				.allowCredentials(true);
+	}
 
 }
