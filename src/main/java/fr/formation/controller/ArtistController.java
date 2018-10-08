aa69a3a9c19be84ccc437add8cc36d0aea284c72
AntonyCo <41469713+AntonyCo@users.noreply.github.com>
2018-10-08 10:19:00 +0200

    Setup architecture (#6)
    ajout de le structure

---
+++ src/main/java/fr/formation/controller/ArtistController.java
@@ -0,0 +1,9 @@
+package fr.formation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/artists/{artistId}")
+public class ArtistController {
+}
---
+++ src/main/java/fr/formation/repository/ArtistDao.java
@@ -0,0 +1,7 @@
+package fr.formation.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ArtistDao {
+}
