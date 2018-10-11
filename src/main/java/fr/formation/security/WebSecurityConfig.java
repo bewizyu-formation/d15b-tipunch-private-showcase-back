3f0e0b64c10e6b87b912dd075bb3ddc2796e8a8f
Antony <antony.correoso@laposte.net>
2018-10-11 10:38:43 +0200

    Add Department REST

--- src/main/java/fr/formation/security/WebSecurityConfig.java
+++ src/main/java/fr/formation/security/WebSecurityConfig.java
@@ -13,53 +13,53 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 /**
  * Configuration of the security
- * 
- *
  */
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	static final String ROLE_ADMIN = "ADMIN";
+    static final String ROLE_ADMIN = "ADMIN";
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http.cors().and().csrf().disable().authorizeRequests()
-			.antMatchers("/v2/api-docs").permitAll()
-			.antMatchers("/swagger-ui.html").permitAll()
-			.antMatchers("/health/").permitAll()
-			.antMatchers(HttpMethod.POST,"/users/").permitAll()
-			.antMatchers(HttpMethod.POST,"/artists/").permitAll()
-			.antMatchers(HttpMethod.POST, "/login").permitAll()
-			.anyRequest().authenticated()
-				.and()	
-			.logout().permitAll()
-				.and()
-			// We filter the api/login requests
-	        .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()),
-	                		UsernamePasswordAuthenticationFilter.class)
-	        // And filter other requests to check the presence of JWT in header
-	        .addFilterBefore(new JwtAuthenticationFilter(),
-	                		UsernamePasswordAuthenticationFilter.class);
-		// @formatter:on
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/health/").permitAll()
+                .antMatchers(HttpMethod.POST, "/users/").permitAll()
+                .antMatchers(HttpMethod.POST, "/artists/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/departments/{deptId}").permitAll()
+                .antMatchers(HttpMethod.GET, "/departments").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout().permitAll()
+                .and()
+                // We filter the api/login requests
+                .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+        // @formatter:on
+    }
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// Allow swagger to be accessed without authentication
-		web.ignoring().antMatchers("/v2/api-docs")//
-				.antMatchers("/swagger-resources/**")//
-				.antMatchers("/swagger-ui.html")//
-				.antMatchers("/configuration/**")//
-				.antMatchers("/webjars/**")//
-				.antMatchers("/public");
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Allow swagger to be accessed without authentication
+        web.ignoring().antMatchers("/v2/api-docs")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public");
+    }
 
-	// Algorithme de hashage du mot de passe
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    // Algorithme de hashage du mot de passe
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
